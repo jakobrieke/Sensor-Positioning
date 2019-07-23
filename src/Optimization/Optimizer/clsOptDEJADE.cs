4075c6cc77f10a13279082fc6e3ddce1a6e06bcc
Jakob <jakobrdev@gmail.com>
2019-07-23 23:42:50 +0200

    Optimize code

--- src/Optimization/Optimizer/clsOptDEJADE.cs
+++ src/Optimization/Optimizer/clsOptDEJADE.cs
@@ -44,10 +44,10 @@ namespace LibOptimization.Optimization
     private int HigherNPercentIndex; // for IsCriterion())
 
     /// <summary>Upper bound(limit solution space)</summary>
-    public double[] UpperBounds { get; set; } = null;
+    public double[] UpperBounds { get; set; }
 
     /// <summary>Lower bound(limit solution space)</summary>
-    public double[] LowerBounds { get; set; } = null;
+    public double[] LowerBounds { get; set; }
 
     // ----------------------------------------------------------------
     // DE parameters
@@ -58,10 +58,10 @@ namespace LibOptimization.Optimization
     public int PopulationSize { get; set; } = 100;
 
     /// <summary>population</summary>
-    private List<LoPoint> m_parents = new List<LoPoint>();
+    private List<LoPoint> _parents = new List<LoPoint>();
 
     /// <summary>archive</summary>
-    private List<LoPoint> m_archive = new List<LoPoint>();
+    private List<LoPoint> _archive = new List<LoPoint>();
 
     /// <summary>Constant raio 0 to 1(Adaptive paramter for muF, muCR)(Default:0.1)</summary>
     public double C { get; set; } = 0.1;
@@ -91,13 +91,13 @@ namespace LibOptimization.Optimization
     {
       try
       {
-        // init meber varibles
+        // Init member variables
         _iteration = 0;
-        m_parents.Clear();
+        _parents.Clear();
         _error.Clear();
-        m_archive.Clear();
+        _archive.Clear();
 
-        // init muF, muCR
+        // Init muF, muCR
         MuCR = 0.5;
         MuF = 0.5;
 
@@ -111,7 +111,7 @@ namespace LibOptimization.Optimization
         }
 
         // generate population
-        for (int i = 0; i <= PopulationSize - 1; i++)
+        for (var i = 0; i <= PopulationSize - 1; i++)
         {
           // initial position
           var array = clsUtil.GenRandomPosition(_func, InitialPosition,
@@ -120,137 +120,137 @@ namespace LibOptimization.Optimization
           // bound check
           var tempPoint = new LoPoint(_func, array);
           if (UpperBounds != null && LowerBounds != null)
+          {
             clsUtil.LimitSolutionSpace(tempPoint, LowerBounds, UpperBounds);
+          }
 
           // save point
-          m_parents.Add(tempPoint);
+          _parents.Add(tempPoint);
         }
 
         // Sort Evaluate
-        m_parents.Sort();
+        _parents.Sort();
 
         // Detect HigherNPercentIndex
-        HigherNPercentIndex = Convert.ToInt32(m_parents.Count * HigherNPercent);
-        if (HigherNPercentIndex == m_parents.Count ||
-            HigherNPercentIndex >= m_parents.Count)
-          HigherNPercentIndex = m_parents.Count - 1;
+        HigherNPercentIndex = Convert.ToInt32(_parents.Count * HigherNPercent);
+        if (HigherNPercentIndex == _parents.Count ||
+            HigherNPercentIndex >= _parents.Count)
+          HigherNPercentIndex = _parents.Count - 1;
       }
       catch (Exception ex)
       {
-        _error.SetError(true, clsError.ErrorType.ERR_INIT);
+        _error.SetError(true, clsError.ErrorType.ERR_INIT, ex.Message);
       }
     }
 
     /// <summary>
     /// Do Iteration
     /// </summary>
-    /// <param name="iteration">Iteration count. When you set zero, use the default value.</param>
+    /// <param name="iteration">Iteration count. When set zero, use the
+    /// default value.</param>
     /// <returns>True:Stopping Criterion. False:Do not Stopping Criterion</returns>
     /// <remarks></remarks>
     public override bool Iterate(int iteration = 0)
     {
-      // Check Last Error
-      if (IsRecentError())
-        return true;
-
-      // Do Iterate
-      if (Iteration <= _iteration)
-        return true;
+      // Check if there is an error and if the optimization has been initialized
+      if (IsRecentError() || Iteration <= _iteration) return true;
+      
       iteration = iteration == 0
         ? Iteration - _iteration - 1
         : Math.Min(iteration, Iteration - _iteration) - 1;
-      for (int iterate = 0; iterate <= iteration; iterate++)
+      
+      for (var iterate = 0; iterate <= iteration; iterate++)
       {
         // Counting generation
         _iteration += 1;
 
         // Sort Evaluate
-        m_parents.Sort();
+        _parents.Sort();
 
         // check criterion
         if (IsUseCriterion)
         {
-          // higher N percentage particles are finished at the time of same evaluate value.
-          if (clsUtil.IsCriterion(EPS, m_parents[0].Eval,
-            m_parents[HigherNPercentIndex].Eval))
+          // higher N percentage particles are finished at the time of same
+          // evaluate value.
+          if (clsUtil.IsCriterion(EPS, _parents[0].Eval,
+            _parents[HigherNPercentIndex].Eval))
             return true;
         }
 
-        // --------------------------------------------------------------------------------------------
+        // ---------------------------------------------------------------------
         // DE process
-        // --------------------------------------------------------------------------------------------
+        // ---------------------------------------------------------------------
         // Mutation and Crossover
         var sumF = 0.0;
         var sumFSquare = 0.0;
-        var sumCR = 0.0;
+        var sumCr = 0.0;
         var countSuccess = 0;
+        
+        // Population iteration
         for (var i = 0; i <= PopulationSize - 1; i++)
         {
           // update F
-          double F;
+          double f;
           while (true)
           {
-            F = clsUtil.CauchyRand(MuF, 0.1);
-            if (F < 0)
+            f = clsUtil.CauchyRand(MuF, 0.1);
+            if (f < 0)
               continue;
-            if (F > 1)
-              F = 1.0;
+            if (f > 1)
+              f = 1.0;
             break;
           }
 
-          // update CR 0 to 1
-          var CR = clsUtil.NormRand(MuCR, 0.1);
-          if (CR < 0)
-            CR = 0.0;
-          else if (CR > 1)
-            CR = 1.0;
+          // Update CR 0 to 1
+          var cr = clsUtil.NormRand(MuCR, 0.1);
+          if (cr < 0) cr = 0.0;
+          else if (cr > 1) cr = 1.0;
 
           // pick pBest
-          LoPoint pbest /* TODO Change to default(_) if this is not a reference type */;
+          LoPoint pBest;
           var pBestIndex =
             Convert.ToInt32(PopulationSize * Random.NextDouble());
           if (pBestIndex <= 2)
           {
-            pbest = Random.NextDouble() > 0.5 ? m_parents[0] : m_parents[1];
+            pBest = Random.NextDouble() > 0.5 ? _parents[0] : _parents[1];
           }
           else if (pBestIndex == PopulationSize)
-            pbest = m_parents[PopulationSize - 1]; // worst
+            pBest = _parents[PopulationSize - 1]; // worst
           else
-            pbest = m_parents[Random.Next(0, pBestIndex)];
+            pBest = _parents[Random.Next(0, pBestIndex)];
 
           // xi,g
-          var xi = m_parents[i];
+          var xi = _parents[i];
 
           // pick xr1,g different parent without i
-          var tempIndex1 = clsUtil.RandomPermutaion(m_parents.Count, i);
+          var tempIndex1 = clsUtil.RandomPermutaion(_parents.Count, i);
           var r1Index = tempIndex1[0];
-          LoPoint p1 = m_parents[r1Index];
+          var p1 = _parents[r1Index];
 
           // pick xr2~,g different parent without i, xr1,g
-          var sumIndex = m_parents.Count + m_archive.Count;
-          var tempIndex2 =
-            clsUtil.RandomPermutaion(0, sumIndex, new[] {i, r1Index});
+          var sumIndex = _parents.Count + _archive.Count;
+          var tempIndex2 = clsUtil.RandomPermutaion(
+            0, sumIndex, new[] {i, r1Index});
           var r2Index = tempIndex2[0];
-          LoPoint
-            p2 = null /* TODO Change to default(_) if this is not a reference type */;
-          if (r2Index >= m_parents.Count)
+          LoPoint p2;
+          if (r2Index >= _parents.Count)
           {
-            r2Index = r2Index - m_parents.Count;
-            p2 = m_archive[r2Index];
+            r2Index -= _parents.Count;
+            p2 = _archive[r2Index];
           }
           else
-            p2 = m_parents[r2Index];
+            p2 = _parents[r2Index];
 
           // Mutation and Crossover
           var child = new LoPoint(ObjectiveFunction);
           var j = Random.Next() % ObjectiveFunction.Dimension();
-          var D = ObjectiveFunction.Dimension() - 1;
+          var d = ObjectiveFunction.Dimension() - 1;
 
           // crossover
-          for (var k = 0; k <= ObjectiveFunction.Dimension() - 1; k++)
+          for (var k = 0; k < ObjectiveFunction.Dimension(); k++)
           {
-            if (Random.NextDouble() < CR || k == D)
-              child[j] = xi[j] + F * (pbest[j] - xi[j]) + F * (p1[j] - p2[j]);
+            if (Random.NextDouble() < cr || k == d)
+              child[j] = xi[j] + f * (pBest[j] - xi[j]) + f * (p1[j] - p2[j]);
             else
               child[j] = xi[k];
             j = (j + 1) % ObjectiveFunction.Dimension(); // next
@@ -260,37 +260,35 @@ namespace LibOptimization.Optimization
 
           // Limit solution space
           clsUtil.LimitSolutionSpace(child, LowerBounds, UpperBounds);
+          
+          if (child.Eval >= _parents[i].Eval) continue;
+          
+          // Add archive
+          _archive.Add(_parents[i].Copy());
 
-          // Survive
-          if (child.Eval < m_parents[i].Eval)
-          {
-            // add archive
-            m_archive.Add(m_parents[i].Copy());
+          // Replace
+          _parents[i] = child;
 
-            // replace
-            m_parents[i] = child;
-
-            // for adaptive parameter
-            sumF += F;
-            sumFSquare += Math.Pow(F, 2);
-            sumCR += CR;
-            countSuccess += 1;
-          }
-        } // population iteration
-
-        // remove archive
-        var removeCount = m_archive.Count - PopulationSize;
-        if (removeCount > 0)
-        {
-          clsUtil.RandomizeArray(ref m_archive);
-          m_archive.RemoveRange(m_archive.Count - removeCount, removeCount);
+          // For adaptive parameter
+          sumF += f;
+          sumFSquare += Math.Pow(f, 2);
+          sumCr += cr;
+          countSuccess += 1;
         }
 
-        // calc muF, muCR
+        // Remove archive
+        var removeCount = _archive.Count - PopulationSize;
+        if (removeCount > 0)
+        {
+          clsUtil.RandomizeArray(ref _archive);
+          _archive.RemoveRange(_archive.Count - removeCount, removeCount);
+        }
+
+        // Calculate muF, muCR
         if (countSuccess > 0)
         {
           // μCR = (1 − c) · μCR + c · meanA(SCR)
-          MuCR = (1 - C) * MuCR + C * (sumCR / countSuccess);
+          MuCR = (1 - C) * MuCR + C * (sumCr / countSuccess);
           // μF = (1 − c) · μF + c · meanL (SF)
           MuF = (1 - C) * MuF + C * (sumFSquare / sumF);
         }
@@ -309,10 +307,8 @@ namespace LibOptimization.Optimization
     /// </summary>
     /// <returns>Best point class</returns>
     /// <remarks></remarks>
-    public override LoPoint Result
-    {
-      get { return clsUtil.GetBestPoint(m_parents, true); }
-    }
+    public override LoPoint Result => 
+      clsUtil.GetBestPoint(_parents, true);
 
     /// <summary>
     /// Get recent error
@@ -332,9 +328,6 @@ namespace LibOptimization.Optimization
     /// <remarks>
     /// for Debug
     /// </remarks>
-    public override List<LoPoint> Results
-    {
-      get { return m_parents; }
-    }
+    public override List<LoPoint> Results => _parents;
   }
 }
