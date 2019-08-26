6dc90d65221594868464873923b8ef87b861e312
Jakob <jakobrdev@gmail.com>
2019-08-26 23:57:05 +0200

    Add documentation

--- src/Optimization/Abstract/Objective.cs
+++ src/Optimization/Abstract/Objective.cs
@@ -7,8 +7,19 @@ namespace Optimization
   {
     public uint Evaluations;
     
+    /// <summary>
+    /// Evaluates the objective function at a given position.
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns></returns>
     public abstract double Eval(Vector position);
 
+    /// <summary>
+    /// Converts an objective function into a lambda function (function
+    /// delegate). 
+    /// </summary>
+    /// <param name="func"></param>
+    /// <returns></returns>
     public static implicit operator Func<double[], double>(Objective func)
     {
       return position => func.Eval(position);
