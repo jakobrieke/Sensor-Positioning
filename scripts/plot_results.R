df6d4c3df08819c70600681ca8e499309e0d68c5
Jakob <jakobrdev@gmail.com>
2019-10-08 22:52:07 +0200

    Add convergence plots

--- scripts/plot_results.R
+++ scripts/plot_results.R
@@ -1,5 +1,8 @@
-data <- read.csv("max-area-results.csv", colClasses=c("Algorithm"="character"))
+data <- read.csv(
+  "max-area-results.csv", 
+  colClasses=c("Algorithm"="character", "Changes"="character"))
 data <- data[order(data$Agents, data$Obstacles),]
+data_ma <- read.csv("marked-area-results.csv", colClasses=c("Algorithm"="character"))
 
 # -- Plot area graph: [1, 2, ..., 11] x 11 (agents times obstacles)
 results_jade <- subset(data, Obstacles==11 & Algorithm==" JADE")
@@ -31,18 +34,39 @@ box()
 axis(1, at=seq(2, 32, by=3), labels=1:11, tick=TRUE, tck=-0.03)
 
 # -- Plot convergance graph: [1, 2, ..., 11] x 11 (agents times obstacles)
-library("XML")
-library("methods")
+changes_jade <- lapply(strsplit(results_jade$Changes, ";"), 
+                  function(y) sapply(y, function(x) as.numeric(x)))
+changes_spso <- lapply(strsplit(results_spso$Changes, ";"), 
+                       function(y) sapply(y, function(x) as.numeric(x)))
 
-parse_changes <- function(x)
-{
-  str_tuple <- strsplit(gsub("\\[|\\]| ", "", x), ",")[[1]]
-  cbind(as.integer(str_tuple[[1]]), as.numeric(str_tuple[[2]]))
+plot(changes_spso[[1]], type = "l", col = "lightblue", 
+     ylim = c(0, 30),
+     xlab = "Iteration", ylab = "Ungesehene Fläche", 
+     main = "")
+for (i in 1:length(changes_jade)) {
+  lines(changes_jade[[i]], type="l", col = "brown1")
 }
-
-doc <- xmlParse(file = "../results/1-agent-1-obstacle-JADE/run-2019-10-06T06-27-44Z138.log")
-r1 <- xmlToList(xpathApply(doc, "//iteration[@i='600']/changes/text()")[[1]])
-r2 <- as.vector(strsplit(r1,"],"), mode="list")[[1]]
-changes <- sapply(r2, parse_changes)
+for (i in 2:length(changes_spso)) {
+  lines(changes_spso[[i]], type="l", col = "lightblue")
+}
+legend(x=600, y=30, c("JADE", "SPSO-06"), cex=.8, 
+       col=c("brown1", "lightblue"), lwd=c(1, 1), xjust=1)
 
 # -- Plot convergance graph: 1x1, 2x2, ..., 11x11 (agents times obstacles)
+changes_jade_2 <- lapply(strsplit(results_jade_2$Changes, ";"), 
+                       function(y) sapply(y, function(x) as.numeric(x)))
+changes_spso_2 <- lapply(strsplit(results_spso_2$Changes, ";"), 
+                       function(y) sapply(y, function(x) as.numeric(x)))
+
+plot(changes_spso_2[[1]], type = "l", col = "lightblue", 
+     ylim = c(0, 25),
+     xlab = "Iteration", ylab = "Ungesehene Fläche", 
+     main = "")
+for (i in 1:length(changes_jade_2)) {
+  lines(changes_jade_2[[i]], type="l", col = "brown1")
+}
+for (i in 2:length(changes_spso_2)) {
+  lines(changes_spso_2[[i]], type="l", col = "lightblue")
+}
+legend(x=600, y=25, c("JADE", "SPSO-06"), cex=.8, 
+       col=c("brown1", "lightblue"), lwd=c(1, 1), xjust=1)
