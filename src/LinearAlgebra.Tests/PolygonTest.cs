cb03e968332b0368feacb461ad234ecc89df1503
Jakob <jakobrdev@gmail.com>
2020-01-08 18:54:21 +0100

    Fix “Polygon difference and union do not handle holes”; Update documentation; Optimise code structure

--- src/LinearAlgebra.Tests/PolygonTest.cs
+++ src/LinearAlgebra.Tests/PolygonTest.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using ClipperLib;
 using NUnit.Framework;
+using Poly2Tri.Triangulation;
+using Poly2Tri.Triangulation.Delaunay.Sweep;
+using Poly2Tri.Triangulation.Polygon;
 
 namespace LinearAlgebra
 {
@@ -42,8 +46,89 @@ namespace LinearAlgebra
       // + 0,5*0,1 - 0,5*0,5
       // + 0,5*0,1 - 0,1*0,1) / 2
       
+      // -- Test regular polygons
+      
       Assert.AreEqual(Polygon.Area(_b), 0.16);
       Assert.AreEqual(Polygon.Area(_c), 1);
+      
+      // -- Test area with holes
+      
+      var clipper = new Clipper();
+      clipper.AddPath(Polygon.ToIntPoint(_a), PolyType.ptSubject, true);
+      clipper.AddPath(Polygon.ToIntPoint(_c), PolyType.ptSubject, true);
+      clipper.AddPath(Polygon.ToIntPoint(_b), PolyType.ptClip, true);
+      clipper.AddPath(Polygon.ToIntPoint(_d), PolyType.ptClip, true);
+
+      var solution = new PolyTree();
+      clipper.Execute(ClipType.ctDifference, solution, PolyFillType.pftNonZero);
+      Assert.AreEqual(Polygon.Area(solution), 1.68);
+    }
+
+    [Test]
+    public void TestDifference()
+    {
+      var difference = Polygon.Difference(new[] {_a, _c}, new[] {_b});
+      Assert.AreEqual(difference.Count, 9);
+
+      foreach (var polygon in difference) Console.WriteLine(polygon);
+
+      Assert.AreEqual(
+        Polygon.FromList(new []{1, 1, 0, 1, 0.5, 0.5}.ToList()),
+        difference[0]);
+      Assert.AreEqual(
+        Polygon.FromList(new []{0, 1, 0.1, 0.5, 0.5, 0.5}.ToList()),
+        difference[1]);
+      Assert.AreEqual(
+        Polygon.FromList(new []{0.1, 0.5, 0, 1, 0, 0}.ToList()),
+        difference[2]);
+      Assert.AreEqual(
+        Polygon.FromList(new []{0, 0, 0.1, 0.1, 0.1, 0.5}.ToList()),
+        difference[3]);
+      Assert.AreEqual(
+        Polygon.FromList(new []{0.5, 0.1, 0.1, 0.1, 0, 0}.ToList()),
+        difference[4]);
+      Assert.AreEqual(
+        Polygon.FromList(new []{1, 0, 0.5, 0.1, 0, 0}.ToList()),
+        difference[5]);
+      Assert.AreEqual(
+        Polygon.FromList(new []{0.5, 0.5, 0.5, 0.1, 1, 0}.ToList()),
+        difference[6]);
+      Assert.AreEqual(
+        Polygon.FromList(new []{0.5, 0.5, 1, 0, 1, 1}.ToList()),
+        difference[7]);
+      Assert.AreEqual(
+        Polygon.FromList(new []{0.0, 0, -1, 0, -1, -1, 0, -1}.ToList()),
+        difference[8]);
+    }
+
+    /// <summary>
+    /// Test how the Poly2Trie API works.
+    /// </summary>
+    [Test]
+    public void TestPoly2Trie()
+    {
+      var polygon = new Poly2Tri.Triangulation.Polygon.Polygon(new[]
+      {
+        new PolygonPoint(0, 0), new PolygonPoint(1, 0),
+        new PolygonPoint(1, 1), new PolygonPoint(0, 1)
+      });
+      var hole1 = new Poly2Tri.Triangulation.Polygon.Polygon(new []
+      {
+        new PolygonPoint(0.1, 0.1), new PolygonPoint(0.2, 0.1),
+        new PolygonPoint(0.2, 0.2), new PolygonPoint(0.1, 0.2)
+      });
+      var hole2 = new Poly2Tri.Triangulation.Polygon.Polygon(new []
+      {
+        new PolygonPoint(0.4, 0.4), new PolygonPoint(0.5, 0.4),
+        new PolygonPoint(0.5, 0.5), new PolygonPoint(0.4, 0.5)
+      });
+      polygon.AddHole(hole1);
+      polygon.AddHole(hole2);
+      Poly2Tri.P2T.Triangulate(polygon);
+      foreach (var triangle in polygon.Triangles)
+      {
+        Console.WriteLine(triangle);
+      }
     }
   }
 }
--- src/LinearAlgebra/Polygon.cs
+++ src/LinearAlgebra/Polygon.cs
@@ -2,14 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using ClipperLib;
+using Poly2Tri.Triangulation.Polygon;
+using Poly2TriePolygon = Poly2Tri.Triangulation.Polygon.Polygon;
 
 namespace LinearAlgebra
 {
-  public class Polygon : List<Vector2>, IGeometryObject 
+  public class Polygon : List<Vector2>, IGeometryObject
   {
-    public Polygon() {}
+    public Polygon()
+    {
+    }
 
-    public Polygon(IEnumerable<Vector2> collection) : base(collection) {}
+    public Polygon(IEnumerable<Vector2> collection) : base(collection)
+    {
+    }
 
     public static bool operator ==(Polygon p1, Polygon p2)
     {
@@ -47,6 +53,26 @@ namespace LinearAlgebra
       
       return Math.Abs(result / 2);
     }
+    
+    public static double Area(PolyNode node)
+    {
+      var area = Area(FromIntPoint(node.Contour));
+      if (node.IsHole) area *= -1;
+
+      area += node.Childs.Sum(Area);
+
+      return area;
+    }
+    
+    /// <summary>
+    /// Calculate the area of a list of polygons.
+    /// </summary>
+    /// <param name="polygons"></param>
+    /// <returns></returns>
+    public static double Area(IEnumerable<Polygon> polygons)
+    {
+      return polygons.Sum(Area);
+    }
 
     /// <summary>
     /// Calculate the union of a list of any type of polygon.
@@ -55,7 +81,7 @@ namespace LinearAlgebra
     /// <param name="closed"></param>
     /// <returns></returns>
     public static List<Polygon> Union(
-      List<Polygon> polygons, bool closed = true)
+      IEnumerable<Polygon> polygons, bool closed = true)
     {
       var clipper = new Clipper();
 
@@ -66,26 +92,20 @@ namespace LinearAlgebra
         clipper.AddPath(shadowPath, PolyType.ptSubject, closed);
       }
 
-      var solution = new List<List<IntPoint>>();
+      var solution = new PolyTree();
 
       clipper.Execute(ClipType.ctUnion, solution,
         PolyFillType.pftPositive, PolyFillType.pftPositive);
 
       var result = new List<Polygon>();
-
-      solution.ForEach(polygon =>
-      {
-        var shadowPolygon = FromIntPoint(polygon);
-        shadowPolygon.Add(shadowPolygon[0]);
-        result.Add(shadowPolygon);
-      });
-
+      foreach (var child in solution.Childs) RemoveHoles(child, ref result);
       return result;
     }
 
     /// <summary>
-    /// Calculate the intersection between two polygons of any type.
+    /// Calculate the intersection between two polygons
     /// </summary>
+    /// <remarks>The polygons are not supposed to contain holes.</remarks>
     /// <param name="polygon1"></param>
     /// <param name="polygon2"></param>
     /// <param name="closed"></param>
@@ -121,13 +141,14 @@ namespace LinearAlgebra
     /// <summary>
     /// Calculate the intersection of two lists of polygons.
     /// </summary>
+    /// <remarks>The polygons are not supposed to contain holes.</remarks>
     /// <param name="polygons1"></param>
     /// <param name="polygons2"></param>
     /// <param name="closed"></param>
     /// <returns></returns>
     public static List<Polygon> Intersection(
-      List<Polygon> polygons1,
-      List<Polygon> polygons2, bool closed = true)
+      IEnumerable<Polygon> polygons1,
+      IEnumerable<Polygon> polygons2, bool closed = true)
     {
       var clipper = new Clipper();
 
@@ -159,70 +180,108 @@ namespace LinearAlgebra
       return result;
     }
 
+    /// <summary>
+    /// Calculate the polygon difference between a list of subject polygons
+    /// and a list of clip polygons.
+    /// </summary>
+    /// <remarks>The clips polygons shall not be overlapping.</remarks>
+    /// <param name="subjects"></param>
+    /// <param name="clips"></param>
+    /// <param name="closed"></param>
+    /// <returns></returns>
     public static List<Polygon> Difference(
-      List<Polygon> polygons1,
-      List<Polygon> polygons2, bool closed = true)
+      IEnumerable<Polygon> subjects,
+      IEnumerable<Polygon> clips, bool closed = true)
     {
       var clipper = new Clipper();
 
-      foreach (var shadow in polygons1)
+      foreach (var polygon in subjects)
       {
-        clipper.AddPath(ToIntPoint(shadow),
-          PolyType.ptSubject, closed);
+        clipper.AddPath(ToIntPoint(polygon), PolyType.ptSubject, closed);
       }
 
-      foreach (var shadow in polygons2)
+      foreach (var polygon in clips)
       {
-        clipper.AddPath(ToIntPoint(shadow),
-          PolyType.ptClip, closed);
+        clipper.AddPath(ToIntPoint(polygon), PolyType.ptClip, closed);
       }
 
-      var solution = new List<List<IntPoint>>();
-
+      var solution = new PolyTree();
       clipper.Execute(ClipType.ctDifference, solution,
         PolyFillType.pftNonZero);
 
       var result = new List<Polygon>();
-      foreach (var path in solution)
-      {
-        var polygon = FromIntPoint(path);
-        polygon.Add(polygon[0]);
-        result.Add(polygon);
-      }
-
+      foreach (var child in solution.Childs) RemoveHoles(child, ref result);
       return result;
     }
-    
+
     /// <summary>
-    /// 
+    /// Calculate the difference between a subject polygon and a clip polygon
+    /// (subject - clip).
     /// </summary>
-    /// <param name="polygon1"></param>
-    /// <param name="polygon2"></param>
+    /// <param name="subject"></param>
+    /// <param name="clip"></param>
     /// <returns></returns>
-    public static List<Polygon> Difference(Polygon polygon1, Polygon polygon2)
+    public static List<Polygon> Difference(Polygon subject, Polygon clip)
     {
       var clipper = new Clipper();
-      clipper.AddPath(ToIntPoint(polygon1),
-        PolyType.ptSubject, true);
-      clipper.AddPath(ToIntPoint(polygon2),
-        PolyType.ptClip, true);
+      clipper.AddPath(ToIntPoint(subject), PolyType.ptSubject, true);
+      clipper.AddPath(ToIntPoint(clip), PolyType.ptClip, true);
 
-      var solution = new List<List<IntPoint>>();
-      clipper.Execute(ClipType.ctDifference, solution,
-        PolyFillType.pftNonZero);
+      var solution = new PolyTree();
+      clipper.Execute(ClipType.ctDifference, solution, PolyFillType.pftNonZero);
 
-      return solution.Select(FromIntPoint).ToList();
+      var result = new List<Polygon>();
+      foreach (var child in solution.Childs) RemoveHoles(child, ref result);
+      return result;
     }
 
-
     /// <summary>
-    /// Calculate the area of a list of polygons.
+    /// A recursive function to remove holes from a PolyTree by triangulating
+    /// the polygon.
+    /// The result is stored inside a flat polygon list.
     /// </summary>
-    /// <param name="polygons"></param>
-    /// <returns></returns>
-    public static double Area(List<Polygon> polygons)
+    /// <param name="node">The PolyNode to remove holes from.</param>
+    /// <param name="result">A list to store the result in.</param>
+    private static void RemoveHoles(PolyNode node, ref List<Polygon> result)
     {
-      return polygons.Sum(Area);
+      // -- Check if the polygon contains holes
+      
+      if (node.ChildCount == 0)
+      {
+        result.Add(FromIntPoint(node.Contour));
+        return;
+      }
+
+      // -- Triangulate the polygon if it contains holes
+      
+      var poly2TriPolygon = (Poly2TriePolygon) FromIntPoint(node.Contour);
+      foreach (var hole in node.Childs)
+      {
+        if (!hole.IsHole)
+        {
+          RemoveHoles(hole, ref result);
+        }
+        poly2TriPolygon.AddHole(
+          (Poly2TriePolygon) FromIntPoint(hole.Contour));
+          
+        foreach (var child in hole.Childs)
+        {
+          RemoveHoles(child, ref result);
+        }
+      }
+      Poly2Tri.P2T.Triangulate(poly2TriPolygon);
+      
+      // -- Add the generated triangles to the result 
+      
+      foreach (var triangle in poly2TriPolygon.Triangles)
+      {
+        result.Add(new Polygon(new []
+        {
+          new Vector2(triangle.Points.Item0.X, triangle.Points.Item0.Y), 
+          new Vector2(triangle.Points.Item1.X, triangle.Points.Item1.Y), 
+          new Vector2(triangle.Points.Item2.X, triangle.Points.Item2.Y) 
+        }));
+      }
     }
 
     /// <summary>
@@ -230,7 +289,7 @@ namespace LinearAlgebra
     /// </summary>
     /// <param name="polygon"></param>
     /// <returns></returns>
-    private static List<IntPoint> ToIntPoint(Polygon polygon)
+    public static List<IntPoint> ToIntPoint(Polygon polygon)
     {
       return polygon.Select(vertex => (IntPoint) vertex).ToList();
     }
@@ -240,17 +299,48 @@ namespace LinearAlgebra
     /// </summary>
     /// <param name="polygon"></param>
     /// <returns></returns>
-    private static Polygon FromIntPoint(List<IntPoint> polygon)
+    public static Polygon FromIntPoint(List<IntPoint> polygon)
     {
-      return new Polygon(polygon.Select(vertex => 
+      return new Polygon(polygon.Select(vertex =>
         (Vector2) vertex).ToList());
     }
+    
+    public static explicit operator Poly2TriePolygon(Polygon polygon)
+    {
+      return new Poly2TriePolygon(
+        polygon.Select(vertex => new PolygonPoint(vertex.X, vertex.Y)));
+    }
+    
+    public static explicit operator Polygon(Poly2TriePolygon polygon)
+    {
+      var result = new Polygon();
+      for (var i = 0; i < polygon.Count; i++)
+      {
+        result.Add(new Vector2(polygon[i].X, polygon[i].Y));
+      }
+      return result;
+    }
 
+    public static void PrintPolyNode(PolyNode node, uint depth = 0)
+    {
+      for (var i = 0; i < depth; i++) Console.Write("  ");
+      Console.Write(node.IsHole ? "Hole" : "Solid");
+      Console.Write(", Children: " + node.ChildCount);
+      Console.Write(", Area: " + Area(node));
+      
+      Console.WriteLine();
+      
+      foreach (var child in node.Childs)
+      {
+        PrintPolyNode(child, depth + 1);
+      }
+    }
+    
     public override string ToString()
     {
       return string.Join("; ", this);
     }
-    
+
     public List<double> ToList()
     {
       var result = new List<double>(Count * 2);
@@ -260,6 +350,27 @@ namespace LinearAlgebra
         result.Add(vector.Y);
       }
 
+      return result;
+    }
+
+    public static Polygon FromList(List<double> xyList)
+    {
+      if (xyList.Count < 6)
+      {
+        throw new ArgumentException(
+          "xyList must contain at least values for three points.");
+      }
+      if (xyList.Count % 2 == 1)
+      {
+        throw new ArgumentException(
+          "xyList must contain pairs of x and y.");
+      }
+
+      var result = new Polygon();
+      for (var i = 0; i < xyList.Count; i += 2)
+      {
+        result.Add(new Vector2(xyList[i], xyList[i + 1]));
+      }
       return result;
     }
   }
