c6db903858f5045e456f872aeebd13e569132d46
Jakob <jakobrdev@gmail.com>
2020-01-12 12:55:36 +0100

    Fix incorrect comma for percent; Update documentation; Remove deprecated code; Simplify format of sensors.py and global_best.py

--- scripts/get_results_no_obstacles.py
+++ scripts/get_results_no_obstacles.py
@@ -1,28 +1,34 @@
-# Generate data:
-# 
-# ./scripts/run 3.1.0 configurations/single-sensor/no-obstacles-SPSO-06.conf 5000 no-obstacles-SPSO-06
-# ./scripts/run 3.1.0 configurations/single-sensor/no-obstacles-JADE.conf 5000 no-obstacles-JADE
-# Aggregate data:
-# 
-# grep -nr "<global-best>" run* > global_best.py
-# grep -nr "<sensors>" run* > sensors.py
-# grep -nr "<changes>" run* > changes.py
+# Usage:
+#
+# Generate data with
+# > run_scenario <build-dir> <output-dir>
+# > run_scenario <build-dir> <output-dir> <config_file>
+# - See configs/test_configs.py for an example of a config_file
+#
+# Aggregate data
+# > grep -nr "<global-best>" run* > global_best.py
+# > grep -nr "<sensors>" run* > sensors.py
+# > grep -nr "<changes>" run* > changes.py
+#
+# Cleanup data
+# - remove every line i % 2 == 0
+# - in sensors.py create a list of floats called "sensors"
+# - in global_best.py create a list of float lists called "global_bests"
+#
+# Start a python shell inside the directory of sensors.py and global_best.py,
+# copy and execute the following script inside the shell
+#
 
 import global_best as gb
 import sensors
-# import changes
 
 # -- Check if results seem ok
 
-results = [t[1] for t in gb.global_bests if True]
+if sum(gb.global_bests) / len(gb.global_bests) > 12.1:
+    print("Error in data: Average final area is too big!")
+    exit()
 
-if sum(results) / len(results) > 12.1:
-  print("Error in data: Average final area is too big!")
-  exit()
-
-# -- 
-
-results = [d['final'] for d in sensors.sensors]
+results = sensors.sensors
 
 # 28°, 151°, 208°, 331°
 possible_angles = [28, 151, 208, 331]
@@ -30,41 +36,41 @@ occurences = [0, 0, 0, 0]
 allowed_deviation = 3
 
 for sensors in results:
-  i = 0
-  for degree in possible_angles:
-    left = degree - allowed_deviation
-    right = degree + allowed_deviation
-    
-    # -- Cleanup values to be in range [0, 360)
-    value = sensors[0][2]
-    
-    while value < 0:
-      value += 360
-    value = value % 360
-    
-    if left <= value <= right:
-      occurences[i] += 1
-      break
-    if i == 3:
-      print("Error in data: " + str(value) + "° exceeds allowed boundries (" + str(sensors[0]) + ")")
-      break
-    i += 1
+    i = 0
+    for degree in possible_angles:
+        left = degree - allowed_deviation
+        right = degree + allowed_deviation
+
+        # -- Cleanup values to be in range [0, 360)
+        value = sensors[0][2]
+
+        while value < 0:
+            value += 360
+        value = value % 360
+
+        if left <= value <= right:
+            occurences[i] += 1
+            break
+        if i == 3:
+            print("Error in data: " + str(
+                value) + "° exceeds allowed boundries (" + str(
+                sensors[0]) + ")")
+            break
+        i += 1
+
+# -- Print statistic
 
 i = 0
 for angle in possible_angles:
-  result = 10 * occurences[i] / len(results)
-  print(str(angle) + "°: " + str(result) + "%")
-  i += 1
+    result = 100 * occurences[i] / len(results)
+    print(str(angle) + "°: " + str(result) + "%")
+    i += 1
+
+# -- Print Latex formatted output
 
 latex_line = ""
 for occurence in occurences:
-  result = round(100 * occurence / len(results), 2)
-  latex_line += "& " + str(result) + "\\pct "
+    result = round(100 * occurence / len(results), 2)
+    latex_line += "& " + str(result) + "\\pct "
 
 print(latex_line)
-
-# Cleaning data "changes"
-# Input:
-# run-2019-09-16T11-12-02Z324.log:99:<changes>[..., [252, 3.37507799486048E-14], [255, 1.06581410364015E-14], [258, 3.5527136788005E-15], [259, 2.48689957516035E-14], ...]</changes>
-# Output:
-# [..., [252, 3.37507799486048E-14], [255, 1.06581410364015E-14], [258, 3.5527136788005E-15], [259, 2.48689957516035E-14], ...]
