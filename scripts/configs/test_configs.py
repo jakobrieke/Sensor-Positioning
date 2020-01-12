2a24253067b449ca54eeb52473dc1d0a2f96fe2b
Jakob <jakobrdev@gmail.com>
2020-01-12 14:12:41 +0100

    Update test configs

--- scripts/configs/test_configs.py
+++ scripts/configs/test_configs.py
@@ -1,5 +1,5 @@
 # Legend:
-# 
+#
 # ag := agents
 # obs := obstacles
 # alg := algorithm
@@ -11,7 +11,7 @@
 # id := used for output directory and configuration file name
 
 configs = [
-  {"ag": 1, "obs": 1, "alg": "JADE",       "ma": [[0, 0], [2, 0], [2, 1], [0, 1]], "dist": -1, "rot": -1, "reps": 100, "its": 50, "id": "1-agent-1-obstacle-ma-ll-JADE"},
-  {"ag": 1, "obs": 1, "alg": "SPSO-2006",  "ma": [], "dist": -1, "rot": -1, "reps": 3, "its": 100, "id": "1-agent-1-obstacle-ma-ll-SPSO-2006"},
-  {"ag": 2, "obs": 2, "alg": "JADE",       "ma": [], "dist": 3, "rot": -1, "reps": 5, "its": 20, "id": "2-agents-2-obstacles-ma-ll-JADE"},
+  {"ag": 1, "obs": 1, "alg": "JADE",       "ma": [[0, 0], [2, 0], [2, 1], [0, 1]], "dist": -1, "rot": -1, "reps": 10, "its": 50, "id": "test-1"},
+  {"ag": 1, "obs": 1, "alg": "SPSO-2006",  "ma": [], "dist": -1, "rot": -1, "reps": 3, "its": 100, "id": "test-2"},
+  {"ag": 2, "obs": 2, "alg": "JADE",       "ma": [], "dist": 3, "rot": -1, "reps": 5, "its": 20, "id": "test-3"},
 ]
