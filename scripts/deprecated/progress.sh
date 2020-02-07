072ea8d55775b20cf8c7c9f1297052e9655eb7df
Jakob <jakobrdev@gmail.com>
2020-02-07 10:53:48 +0100

    Move data processing scripts into separate project

--- scripts/deprecated/progress.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in $(ls ~/.charlie/SensorPositioning.StaticSpSimulation)
-do
-    dir="$HOME/.charlie/SensorPositioning.StaticSpSimulation/$i"
-	v=$(find $dir -type f -name '*.log' |wc -l |xargs)
-	# v=$(ls -1q ~/.charlie/SensorPositioning.StaticSpSimulation/$i/*.log |wc -l |xargs)
-	echo "$i - $v"
-done
